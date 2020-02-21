a90bf87d0bc20d64b9e3e071ac1784eef2db92ca
Yurii <yurii.androsiuk@gmail.com>
2020-02-21 17:21:59 +0200

    feat: func working

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
+exports.min = function min(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    return Math.min(...array);
 
-exports.min = function min (array) {
-  return 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    return Math.max(...array);
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    if (arguments.length === 0 || array.length === 0) return 0;
+    let sum = 0;
+    array.forEach(a => sum += a);
+    let res = sum / (array.length);
+    return res;
 }
